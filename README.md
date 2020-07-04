a420887e9e84ed254f484af87179b4c4d7d3268c
Kisophi <akiramatsumoto@live.com>
2020-07-04 01:39:59 -0300

    Add CSS Materialize

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Eventos
 Sistema de Eventos com lista de Convidados
+
+
+# SpringBoot
+- Thymeleaf
+- Spring DevOps 
+- Spring Web
+- Spring JPA
+- MySQL
+
+# Materialize setup
+. css/
+   - materialize.css
+   
+. fonts/
+   - roboto/
+
+. js/
+   - materialize.js
+
+
+
+
--- src/main/java/com/akira/main/EventosApplication.java
+++ src/main/java/com/akira/main/EventosApplication.java
@@ -2,8 +2,12 @@ package com.akira.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.akira.controller")
+@EntityScan("com.akira.model")
 public class EventosApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +15,4 @@ public class EventosApplication {
 	}
 
 }
+	
