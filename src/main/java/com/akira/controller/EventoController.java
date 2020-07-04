f908b563b740920143141b3ed5374cbb015d7ede
Kisophi <akiramatsumoto@live.com>
2020-07-04 02:53:10 -0300

    add Form de cadastro de Eventos

--- src/main/java/com/akira/controller/EventoController.java
+++ src/main/java/com/akira/controller/EventoController.java
@@ -1,14 +1,35 @@
 package com.akira.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.akira.model.Evento;
+import com.akira.repository.EventoRepository;
 
 @Controller
 public class EventoController {
 
+	@Autowired
+	private EventoRepository er;
+	
 	@RequestMapping("/")
 	public String home() {
 		return "evento/index";
+	}
+	
+	//Encaminha para pagina Form de Cadastro de evento
+	@RequestMapping(value = "/salvar",method = RequestMethod.GET)
+	public String paginaSalvar() {
+		return "evento/formSalvar";
+	}
+	
+	//Salva os dados da tela de cadastro
+	@RequestMapping(value = "/salvar",method = RequestMethod.POST)
+	public String salvar(Evento e) {
+		er.save(e);
+		return "evento/formSalvar";
 	}
 	
 }
--- src/main/java/com/akira/main/EventosApplication.java
+++ src/main/java/com/akira/main/EventosApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.akira.controller")
 @EntityScan("com.akira.model")
+@EnableJpaRepositories("com.akira.repository")
 public class EventosApplication {
 
 	public static void main(String[] args) {
